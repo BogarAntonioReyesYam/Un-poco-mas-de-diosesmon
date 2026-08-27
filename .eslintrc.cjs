59b779851817e0d7bf0f1ec1be10919b35d0d5cd
BogarAntonioReyesYam <238365188+BogarAntonioReyesYam@users.noreply.github.com>
2026-08-26 21:23:02 -0600

    Add files via upload

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.3' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react-refresh/only-export-components': 'warn',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+}
